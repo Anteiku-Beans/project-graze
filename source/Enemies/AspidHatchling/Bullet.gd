623e8fa9c7e198e1a2f88493f4431fdc651d96e7
martin-cao <anteiku.beans@gmail.com>
2020-03-07 16:40:02 +1100

    Aspid bullets die on collision

--- source/Enemies/AspidHatchling/Bullet.gd
+++ source/Enemies/AspidHatchling/Bullet.gd
@@ -2,15 +2,27 @@ extends GrazeableHitbox
 
 var direction: Vector2
 var move_speed: float = 100.0
-var spin_speed: float = 4 * PI
 
 func _ready():
 	set_physics_process(false)
+	self.connect("body_entered", self, "_on_body_entered")
 
 func _physics_process(delta):
 	self.global_position += direction * move_speed * delta
-	self.rotate(spin_speed * delta)
 
 func set_direction(new_direction: Vector2) -> void:
 	direction = new_direction
 	set_physics_process(true)
+
+func _on_body_entered(body):
+	die()
+
+func die():
+	set_physics_process(false)
+	self.monitoring = false
+	self.monitorable = false
+	$Sprite.play("die")
+	$Sprite.connect("animation_finished", self, "_on_death_animation_finished")
+
+func _on_death_animation_finished():
+	queue_free()
