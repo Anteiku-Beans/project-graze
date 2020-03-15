d6b8581fa7b621be27b6212207d32c25a7631dda
martin-cao <anteiku.beans@gmail.com>
2020-03-15 14:32:51 +1100

    Implement player facing direction

--- source/Player/Player.gd
+++ source/Player/Player.gd
@@ -1,5 +1,10 @@
 extends KinematicBody2D
 
+const _facing_RIGHT = 1
+const _facing_LEFT = -1
+
+var _facing = _facing_RIGHT
+
 func _ready() -> void:
 	Player.set_player(self)
 
@@ -7,3 +12,25 @@ func _ready() -> void:
 #       rapidly switching between move and fall during movement
 func is_on_floor():
 	return test_move(self.transform, Vector2.DOWN)
+
+func update_facing(direction):
+	var new_direction: int = 0
+	
+	if direction is Vector2:
+		new_direction = sign(direction.x)
+	elif direction is int or direction is float:
+		new_direction = sign(direction)
+	
+#	Check if new direction is 0
+	if new_direction == 0:
+		return
+	
+	self._facing = new_direction
+
+
+func get_facing_int():
+	return _facing
+
+
+func get_facing_vector2():
+	return Vector2(_facing, 0)
--- source/Player/states/Free.gd
+++ source/Player/states/Free.gd
@@ -1,5 +1,8 @@
 extends State
 
+const FACING_LEFT = -1
+const FACING_RIGHT = 1
+
 const MOVE_ACCELERATION = Vector2(1500, 0)
 const MOVE_DAMP = Vector2(2500, 0)
 const MOVE_MAX_SPEED = Vector2(120, 0)
@@ -17,6 +20,7 @@ func _ready():
 
 func physics_process(delta):
 	update_move(delta)
+	owner.update_facing(get_move_direction())
 
 
 func update_move(delta) -> void:
@@ -34,7 +38,14 @@ func get_move_direction() -> Vector2:
 
 func unhandled_input(event):
 	if event.is_action_pressed("dash"):
-		_state_machine.transition_to("Dash", {"direction":Vector2.RIGHT})
+#		Check if direction was pressed with dash
+		var dash_direction: Vector2
+		var x_input = Input.get_action_strength("right") - Input.get_action_strength("left")
+		if x_input != 0:
+			dash_direction = Vector2(x_input, 0)
+		else:
+			dash_direction = owner.get_facing_vector2()
+		_state_machine.transition_to("Dash", {"direction":dash_direction})
 
 
 func _on_hit():
