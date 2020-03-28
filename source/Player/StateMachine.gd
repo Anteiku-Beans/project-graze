4fe7bb759b55ed5aa0a5985f7105cb2fd0776cb4
martin-cao <anteiku.beans@gmail.com>
2020-03-29 10:31:53 +1100

    Delete unused files

--- source/Player/StateMachine.gd
+++ source/Player/StateMachine.gd
@@ -4,4 +4,3 @@ func transition_to(target_state_path: String, data: Dictionary = {}) -> void:
 	if target_state_path == "Dash" and $Dash.is_on_cooldown():
 		return
 	.transition_to(target_state_path, data)
-	print("New state: ", target_state_path)
--- source/Player/states/Idle2.gd
+++
@@ -1,6 +0,0 @@
-extends State
-
-onready var free: = get_parent()
-
-func physics_process(delta):
-	free.physics_process(delta)
