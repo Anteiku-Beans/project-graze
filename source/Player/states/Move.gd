a50aa675d4049af073dbd0147131510b7bad8a70
martin-cao <anteiku.beans@gmail.com>
2020-03-29 17:51:39 +1100

    Add Move Ground Particles

--- source/Player/states/Move.gd
+++ source/Player/states/Move.gd
@@ -1,5 +1,10 @@
 extends State
 
+const GROUND_PARTICLES = preload("res://source/Effects/MoveGroundParticles/MoveGroundParticles.tscn")
+const GROUND_PARTICLES_OFFSET = Vector2(0, -8)
+
+var current_ground_particles
+
 onready var free = get_parent()
 onready var sprite = owner.get_node("Sprite")
 onready var wall_detector = owner.get_node("WallDetector")
@@ -9,6 +14,7 @@ onready var player = owner
 func enter(data: Dictionary = {}):
 	free.enter(data)
 	sprite.request("move")
+	summon_ground_particles()
 
 
 func physics_process(delta: float):
@@ -27,6 +33,7 @@ func physics_process(delta: float):
 
 
 func exit():
+	unsummon_ground_particles()
 	free.exit()
 
 
@@ -35,3 +42,16 @@ func unhandled_input(event: InputEvent):
 		_state_machine.transition_to('Free/Jump')
 		return
 	free.unhandled_input(event)
+
+
+func summon_ground_particles():
+	current_ground_particles = GROUND_PARTICLES.instance()
+	player.add_child(current_ground_particles)
+	current_ground_particles.global_position = player.global_position
+	current_ground_particles.position += GROUND_PARTICLES_OFFSET
+	current_ground_particles.scale.x = player.get_facing_int()
+
+
+func unsummon_ground_particles():
+	if current_ground_particles != null:
+		current_ground_particles.queue_free()
