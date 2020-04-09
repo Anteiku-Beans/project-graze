1ca15b2605daccf9bb1b0d1df17ccad74ed60a37
martin-cao <anteiku.beans@gmail.com>
2020-04-10 08:13:34 +1000

    Clean up Mana node

--- source/Player/Mana.gd
+++ source/Player/Mana.gd
@@ -1,13 +1,24 @@
 extends Node
 
-const INITIAL_MANA = 0.0
-
 export var max_mana: float = 100.0
 
-var mana: float = INITIAL_MANA
+var _current_mana: float = max_mana
+
+signal depleted
+signal maxed
+
+
+func get_mana():
+	return _current_mana
+
 
 func add_mana(amount: float) -> void:
-	mana = min(max_mana, mana + amount)
+	_current_mana = min(max_mana, _current_mana + amount)
+	if _current_mana == max_mana:
+		emit_signal("maxed")
+
 
 func remove_mana(amount: float) -> void:
-	mana = max(0.0, mana - amount)
+	_current_mana = max(0.0, _current_mana - amount)
+	if _current_mana == 0:
+		emit_signal("depleted")
