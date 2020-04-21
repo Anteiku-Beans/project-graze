70e393654ed4ab55d9e111483bd29d77b33be907
martin-cao <anteiku.beans@gmail.com>
2020-04-21 18:46:57 +1000

    Fix Flying Assaulter not summoning hitbox.

--- source/Enemies/Manji/States/FlyingAssaulter__Land.gd
+++ source/Enemies/Manji/States/FlyingAssaulter__Land.gd
@@ -2,14 +2,28 @@ extends Node
 
 signal finished
 
+const DAMAGE_ZONE_FRAME = 0
+
 onready var manji = owner
 onready var sprite = owner.get_node("Sprite")
 onready var fa = get_parent()
+onready var damage_zone = owner.get_node("DamageZone")
 
 
 func enter() -> void:
 	sprite.play("flying_assaulter_land")
 	sprite.connect("animation_finished", self, "_on_animation_finished")
+	sprite.connect("frame_changed", self, "_on_frame_changed")
+	if sprite.frame == DAMAGE_ZONE_FRAME:
+		damage_zone.enable("FlyingAssaulter")
+
+
+func _on_frame_changed() -> void:
+	if sprite.frame == DAMAGE_ZONE_FRAME:
+		damage_zone.enable("FlyingAssaulter")
+	elif sprite.frame > DAMAGE_ZONE_FRAME:
+		damage_zone.disable("FlyingAssaulter")
+		sprite.disconnect("frame_changed", self, "_on_frame_changed")
 
 
 func _on_animation_finished() -> void:
@@ -22,5 +36,6 @@ func physics_process(delta) -> void:
 
 func exit() -> void:
 	sprite.disconnect("animation_finished", self, "_on_animation_finished")
+	sprite.disconnect("frame_changed", self, "_on_frame_changed")
 
 
